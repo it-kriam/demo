e5a86ea32b23e43c32edb88093f03fe3ddf9a0fe
amit-dev101 <amit.airth@gmail.com>
2026-03-11 11:24:09 +0530

    aiobe and nfe

--- src/main/java/com/example/demo/contorller/DemoController.java
+++ src/main/java/com/example/demo/contorller/DemoController.java
@@ -5,24 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.sql.SQLException;
-
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
    @GetMapping("/dummy")
- public String dummyapi(@RequestParam String param) throws FileNotFoundException, SQLException {
-       FileReader file = new FileReader("data.txt");
+ public String dummyapi(@RequestParam String param) {
 
-       // Artificial condition to throw SQLException
-       if (file == null) {
-           throw new SQLException("Database error occurred");
+       String numbers[] = {"10", "20", "ABC", "40"};
+
+       for (int i = 0; i <= numbers.length; i++) {
+
+           int num = Integer.parseInt(numbers[i]); // May throw NumberFormatException
+
+           System.out.println("Number: " + num);
        }
 
-       System.out.println("Processing completed");
+      Integer abc =  Integer.parseInt("ABC");
      return param;
  }
 }
