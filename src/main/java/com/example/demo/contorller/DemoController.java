e382fec082ba9c92c579448b6984546efd725bea
amit-dev101 <amit.airth@gmail.com>
2026-03-11 11:07:20 +0530

    second commit

--- src/main/java/com/example/demo/contorller/DemoController.java
+++ src/main/java/com/example/demo/contorller/DemoController.java
@@ -5,12 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.SQLException;
+
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
    @GetMapping("/dummy")
- public String dummyapi(@RequestParam String param){
+ public String dummyapi(@RequestParam String param) throws FileNotFoundException, SQLException {
+       FileReader file = new FileReader("data.txt");
+
+       // Artificial condition to throw SQLException
+       if (file == null) {
+           throw new SQLException("Database error occurred");
+       }
+
+       System.out.println("Processing completed");
      return param;
  }
 }
